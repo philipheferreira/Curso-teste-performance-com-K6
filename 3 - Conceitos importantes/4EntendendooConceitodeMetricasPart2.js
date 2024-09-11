5c5f5523e215bc031948b5bd3da66fc352fd9ca1
philipheferreira <philipheferreira@gmail.com>
2024-09-11 16:59:01 -0300

    Feat: correction metrica fatal error

--- 3 - Conceitos importantes/4EntendendooConceitodeMetricasPart2.js
+++ 3 - Conceitos importantes/4EntendendooConceitodeMetricasPart2.js
@@ -1,21 +1,27 @@
 import http from 'k6/http';
 import {check} from 'k6';
-//import {Counter}
-
+import { Counter} from 'k6/metrics'; // modulo do k6 especifico para adicionar metricas referente a counters
+import { Gauge } from 'k6/metrics'; // metrica do tipo Gauge sendo importada da biblioteca
+import { Rate } from 'k6/metrics';
 
 export const options = {
-	vus: 1,
+	vus: 3,
 	duration: '3s'
 }
 
 
+// constantes que irão receber as metricas que são puxadas das respectivas bibliotecas
+const chamadas = new Counter('Quantidade de chamdas'); // A constante chamadas ira receber a variavel Counter da biblioteca metrics
+const meuGauge = new Gauge('Tempo bloqueado');
+const meuRate = new Rate('Taxa de requisição 200');
 
 export default function () {
-	const res = http.get('http://test.k6.io/')
+	const res = http.get('http://test.k6.io/');
 
-	check(res, {
-		'status code é 200': (r) => r.status === 200
-	});
+	// declaração de constante previamente declaradas que vão mostrar o resultado das metricas declaradas
+	chamadas.add(1); // variavel que recebe a metrica Counter sendo chamada no cmd;
+	meuGauge.add(res.timings.blocked); // O req acessa o que foi retornado de informação da requisição, acessa a propriedade timings que tem muita informação sobre duração, tempo de bloqueio, tempo de conexão. Em seguida acessa tempo de bloqueio
+	meuRate.add(res.status === 200);
 }
 
 
