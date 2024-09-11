09d551f87d4a145e364f762416deeeb892e404ca
philipheferreira <philipheferreira@gmail.com>
2024-09-11 10:23:59 -0300

    Create module 2, cap 4, 5 and 6

--- 2 - Entendendo os tipos de teste performance/5 - Load Test.js
+++ 2 - Entendendo os tipos de teste performance/5 - Load Test.js
@@ -1,15 +1,22 @@
 // Entenderemos melhor sobre options no modulo 3!!!
 
-/*Carga constante, é para 
-testar o maximo de recursos e disponibilidade até quebrar */
+/*
 
+Funcionalidades do codigo no geral
+- Permite que seu sistema aqueça ou redimensione automaticamente para lidar com o tráfego.
+- Permite que você compare o tempo de resposta entre os estágios de carga baixa e carga nominal.
+
+*/
+
+/*Carga constante: primeira abordagem */
 export const options1 = {
 	vus: 100,
 	duration: '20m',
 };
 
+// carga variavel: segunda abordagem. Consegue modular a quantidade de pessoas que entra e a duração dessa carga
 export const options2 = {
-	stages: [
+	stages: [ // divide em 3 fases dentro do projeto
 		{duration: '5m', target: 100},
 		{duration: '10m', target: 100},
 		{duration: '5m', target: 0},
---
+++ 2 - Entendendo os tipos de teste performance/6 - teste de Stress e Spike test.js
@@ -0,0 +1,17 @@
+/*
+
+						----------------------- STRESS TESTING -------------------------------
+PERGUNTAS
+1 - Como seu sistema se comporta em condições extremas?
+2 - Qual é a capacidade máxima do seu sistema em termos de usuários ou taxa de transferência?
+3 - O ponto de ruptura do seu sistema?
+4 - O sistema se recupera sem intervenção manual após o termino do teste de estresse?
+
+RESPOSTA
+1 - A rapidez com que os mecanismos de dimensionamento automático reagem ao aumento da carga.
+2 - Se houver alguma falha durante os eventos de dimensionamento
+*/
+
+// tem o objetivo de validar arquitetura de uma aplicação, verificando gargalos que podem ser prejudiciais para uma aplicação. Ex: o teste
+// pode mostrar situações de que não está escalando bem, mostrar problemas de auto recuperação
+
