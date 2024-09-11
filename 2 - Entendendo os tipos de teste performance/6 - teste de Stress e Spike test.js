abaca460189afe02eb9c4dae97793aefcbf2518e
philipheferreira <philipheferreira@gmail.com>
2024-09-11 10:44:11 -0300

    Feat: stress test and spike test

--- 2 - Entendendo os tipos de teste performance/6 - teste de Stress e Spike test.js
+++ 2 - Entendendo os tipos de teste performance/6 - teste de Stress e Spike test.js
@@ -15,3 +15,27 @@ RESPOSTA
 // tem o objetivo de validar arquitetura de uma aplicação, verificando gargalos que podem ser prejudiciais para uma aplicação. Ex: o teste
 // pode mostrar situações de que não está escalando bem, mostrar problemas de auto recuperação
 
+
+/*
+					------------------------- SPIKE TESTING --------------------------------
+PERGUNTAS
+1 - Como seu sistema funcionará sob um aumento repentino de tráfego?
+2 - O seu sistema irá se recuperar assim que o tráfego diminuir?
+
+- Tem 4 formas de categorizar: 1 - excelente, 2 - bom, 3 - insatisfatório e 4 - ruim
+
+*/
+
+
+export const options = {
+	stages: [
+		{duration: "10s", target: 100}, // 
+		{duration: "1m", target: 100}, //
+		{duration: "10s", target: 1400}, // Etapa de aceteração. Aumento de usuarios em um curto periodo de tempo
+		{duration: "3m", target: 1400}, //
+		{duration: "10s", target: 100}, // 
+		{duration: "3m", target: 100}, //
+		{duration: "10s", target: 0}, //
+		]
+	}
+}
