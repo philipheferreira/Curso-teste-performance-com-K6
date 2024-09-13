73b43eb8ef0b6d104ab8132bd16d0edf00deb749
philipheferreira <philipheferreira@gmail.com>
2024-09-13 14:54:53 -0300

    Feat: make variables script

---
+++ 3 - Conceitos importantes/9UtilizandoVariaveisDeAmbiente.js
@@ -0,0 +1,9 @@
+// 							---------------------------  VARIAVEIS DE AMBIENTE	--------------------------------------
+
+/*
+as seguintes finalidades são:
+1 - Fornecer uma variável de ambiente para o script.
+2 - Realizar configurações do escopo de options
+
+
+*/
