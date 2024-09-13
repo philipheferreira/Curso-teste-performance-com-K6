5a72ca451ab943e16b2d0cecbec8ff9f88c9e62e
philipheferreira <philipheferreira@gmail.com>
2024-09-13 13:41:41 -0300

    Create : tags script

---
+++ 3 - Conceitos importantes/8ConhecendoTags.js
@@ -0,0 +1,26 @@
+//		ROTULAR COM TAGS
+
+
+/*
+ O que pode ser rotulado com as tags no K6
+1 - Requests (requisições);
+2 - checks (validações);
+3 - Thresolds (limites);
+4 - Métricas customizadas;
+5 - Todas as métricas de um teste;
+
+obs: tags podem ser utilizadas em conjunto com groups
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
