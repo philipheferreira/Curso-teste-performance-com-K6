a2773e1eeb4a5280212e4a515b68f6287fa54d13
philipheferreira <philipheferreira@gmail.com>
2024-09-13 16:35:50 -0300

    Feat: fisnish scenario with duration cmd declarete and duration, vus declarete

--- 3 - Conceitos importantes/9UtilizandoVariaveisDeAmbiente.js
+++ 3 - Conceitos importantes/9UtilizandoVariaveisDeAmbiente.js
@@ -10,6 +10,8 @@ as variaveis de ambiente são declaradas dentro do cmd da pasta pelo K6 podendo 
 
 //		----------------------------------------------	SCRIPT INICIAL	---------------------------------------------
 
+/*
+
 import http from 'k6/http';
 import { sleep } from 'k6';
 
@@ -25,8 +27,11 @@ export default function(){
 	const res = http.get(BASE_URL);
 }
 
+*/
 
 //			----------------------------------------		SCRIPT APÓS TIRAR URL E RECEBER UMA VARIAVEL PELO CMD		---------------------------------
+
+/*
 
 import http from 'k6/http';
 import { sleep } from 'k6';
@@ -47,9 +52,11 @@ export default function(){
 	sleep(1);
 }
 
+*/
 
 // 		----------------------------------------		SCRIPT APÓS RETIRAR O ESCOLPO export const options		--------------------------------------
 
+/*
 
 import http from 'k6/http';
 import { sleep } from 'k6';
@@ -67,3 +74,56 @@ export default function(){
 
 //k6 run -e URL=https://test-api.k6.io/public/crocodiles/ 9UtilizandoVariaveisDeAmbiente.js 
 // ou k6 run --env URL=https://test-api.k6.io/public/crocodiles/ 9UtilizandoVariaveisDeAmbiente.js
+
+*/
+
+
+
+//		-----------------------------------			SCRIPT SEM options declarando duração pelo CMD 		---------------------------------
+
+
+
+/*
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+// caso o export const options seja retirado como um todo o k6 usa os parametros pre-definidos de vus = 1 e duration = 1s
+
+export default function(){
+	const BASE_URL = __ENV.URL;
+	const res = http.get(BASE_URL);
+
+	sleep(1);
+
+}
+
+*/
+
+// o comando a baixo além de definir o valor na variavel URL de ambiente defini o tempo de duração do teste
+//k6 run -e URL=https://test-api.k6.io/public/crocodiles/ 9UtilizandoVariaveisDeAmbiente.js --duration 10s
+
+
+
+//			-------------------------------			SCRIPT SEM options declarando duração e usuarios pelo CMD 		--------------------------------------------
+
+
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+// caso o export const options seja retirado como um todo o k6 usa os parametros pre-definidos de vus = 1 e duration = 1s
+
+export default function(){
+	const BASE_URL = __ENV.URL;
+	const res = http.get(BASE_URL);
+
+	sleep(1);
+
+}
+
+
+// o comando a baixo além de definir o valor na variavel URL de ambiente defini o tempo de duração do teste
+//k6 run -e URL=https://test-api.k6.io/public/crocodiles/ 9UtilizandoVariaveisDeAmbiente.js --duration 10s --vus 30
