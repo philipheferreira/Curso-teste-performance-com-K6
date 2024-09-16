b22981476ede53a4f631e339eccdf704aea2ba18
philipheferreira <philipheferreira@gmail.com>
2024-09-16 13:56:56 -0300

    Feat: gib project part 3 finish

--- 4 - Mão na massa/3ConstruindoTestedeCargaParte3.js
+++ 4 - Mão na massa/3ConstruindoTestedeCargaParte3.js
@@ -22,20 +22,34 @@ Registration e auth: Registro
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-
 export const options = {
-	stages : [{duration: '10s', target: 10}],
-	thresolds: {
-		checks: ['rate > 0.95'],
-		http_req_failed: ['rate < 0.01'],
-		http_req_duration: ['p(95) < 500']
-	}
-};
+    stages: [{ duration: '10s', target: 10 }],
+    thresholds: {
+        checks: ['rate > 0.95'],
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: ['p(95) < 500']
+    }
+}
 
+export default function () {
+    const USER = `${Math.random()}@mail.com`
+    const PASS = 'user123'
+    const BASE_URL = 'https://test-api.k6.io';
 
-export default function(){
-	const BASE_URL = 'https://test-api.k6';
-	const PASS = 'user123';
-	const BASE_URL = 'https://test-api.k6.io';
+    console.log( USER + PASS);
+
+    const res = http.post(`${BASE_URL}/user/register/`, {
+        username: USER,
+        first_name: 'crocrodilo',
+        last_name: 'dino',
+        email: USER,
+        password: PASS
+    });
+
+    check(res, {
+        'sucesso ao registar': (r) => r.status === 201
+    });
+
+    sleep(1)
 }
 
