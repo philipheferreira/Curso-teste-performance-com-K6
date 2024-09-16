0cf1d443970c22ddc80a4fdaaf20faeed3e3b96e
philipheferreira <philipheferreira@gmail.com>
2024-09-16 14:05:12 -0300

    Create: big project part 4 template basic

--- 4 - Mão na massa/4ConstruindoTestedeCargaParte4.js
+++ 4 - Mão na massa/4ConstruindoTestedeCargaParte4.js
@@ -14,3 +14,41 @@
 			- Requisição com falha inferior a 1%
 
 */
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [{ duration: '10s', target: 10 }],
+    thresholds: {
+        checks: ['rate > 0.95'],
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: ['p(95) < 500']
+    }
+}
+
+export default function () {
+    const USER = `${Math.random()}@mail.com`
+    const PASS = 'user123'
+    const BASE_URL = 'https://test-api.k6.io';
+
+    console.log( USER + PASS);
+
+    const res = http.post(`${BASE_URL}/user/register/`, {// metodo post dentro da constant res
+        username: USER,// Infos que serão passadas pelo post
+        first_name: 'crocrodilo',
+        last_name: 'dino',
+        email: USER,// email recebe o user definido dentro do codigo
+        password: PASS// senha recebe pass que é declarada dentro do codigo
+    });
+
+    check(res, {// chamada rest quando retornar 201 mostrara sucesso ao registrar e retornar status
+        'sucesso ao registar': (r) => r.status === 201
+    });
+
+    sleep(1)
+}
+
+
+
+
