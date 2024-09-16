2e67d01f23208c85d40fce98d33e6e82b6f295f6
philipheferreira <philipheferreira@gmail.com>
2024-09-16 13:05:51 -0300

    Create: Plane project

---
+++ 4 - Mão na massa/4ConstruindoTestedeCargaParte4.js
@@ -0,0 +1,16 @@
+/*
+	Registrar e autorizar: login
+		* Realizar o login com um novo usuario
+
+	Criterios:
+		* Stress test
+			- Ramp up 5 VU em 5s
+			- Carga 5 VU por 5s
+			- Ramp up 50 VU em 2s
+			- Carga de 50 VU em 2s
+			- Ramp down 0 VU em 5s
+
+		* Limites:
+			- Requisição com falha inferior a 1%
+
+*/
