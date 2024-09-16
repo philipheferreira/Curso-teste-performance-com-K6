c1a45771f6a47118a2f2c96c2a0299c53ed94fd1
philipheferreira <philipheferreira@gmail.com>
2024-09-16 11:50:58 -0300

    FEat: template functional part 2

--- 4 - Mão na massa/2ConstruindoTestedeCargaParte2.js
+++ 4 - Mão na massa/2ConstruindoTestedeCargaParte2.js
@@ -14,24 +14,30 @@
 */
 
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6'; // O sleep é utilizado para definir que cada  virtual user defina um tempo para que ela realize uma proxima solicitação de requisição
 
 export const options = {
-	vus: 1,
-	duration: '3s',
+	stages: [
+			{duration: '10s', target: 10},
+			{duration: '10s', target: 10},
+			{duration: '10s', target: 0}
+		],
 	thresholds: {
-		checks: ['rate > 0.99']
+		checks: ['rate > 0.99'],
+		http_req_duration: ['p(95) < 200'] // A requisição com o persitivo de 95% deve ter um tempo inferior de 200 milissegundos para funcionar perfeitamente
 	}
 }
 
 export default function(){
-	const BASE_URL = 'https://test-api.k6.io/public/crocodiles/';
+	const BASE_URL = 'https://test-api.k6.io/public/crocodiles/1';
 
 	const res = http.get(BASE_URL);
 
 	check(res, {
 		'status code 200': (r) => r.status === 200
-	})
+	});
+
+	sleep(1)
 }
 
 
