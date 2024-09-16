a683a525e9a93ad7e3dbfe705457dbb47c1501b5
philipheferreira <philipheferreira@gmail.com>
2024-09-16 10:19:49 -0300

    Feat: finish scenarios part 3

--- 3 - Conceitos importantes/11EntendendoScenariosPart3.js
+++ 3 - Conceitos importantes/11EntendendoScenariosPart3.js
@@ -9,10 +9,10 @@
 
 
 /*
+			-----------------------------------------------			PUR-VU-ITERATIONS: 		-------------------------------------------------------
 
-
-
-
+1 - Número especifico de VUs para completar o mesmo número fixo de iterações
+2 - Participar dados de teste entre VUs
 
 
 
@@ -21,6 +21,20 @@
 
 
 /* 
+			------------------------------------------------		CONSTANT-VUS		--------------------------------------------------------
+1 - Número especifico de VUs seja executado em um periodo especificado de tempo
+
+
+
+*/
+
+
+
+/*
+			----------------------------------------------		CONSTANT-ARRIVAL-RATE	--------------------------------------------------------
+
+1 - Executor com foco em métrica como o RPS
+
 
 
 
