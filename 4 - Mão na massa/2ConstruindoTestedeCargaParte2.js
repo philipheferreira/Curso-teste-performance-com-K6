0b2bdc6bdfb25e5c20f2067825d483abfb213068
philipheferreira <philipheferreira@gmail.com>
2024-09-16 11:02:51 -0300

    Feat: part 2 adjust plane project

---
+++ 4 - Mão na massa/2ConstruindoTestedeCargaParte2.js
@@ -0,0 +1,14 @@
+/* 
+			--------------------------------------------			MÃO NA MASSA PART 2			--------------------------------------------
+	Public API: Exemplo 2
+		- BUscar crocodilo por id
+
+	Critérios:
+		* Performance test
+			- Ramp up 10 VU e, 10s
+			- Carga 10 VU por 10s
+			- Ramp down 0 VU em 10s
+		* Limites:
+			- Requisição com sucesso > 95%
+			- Tempo requisição p(90) < 200
+*/
