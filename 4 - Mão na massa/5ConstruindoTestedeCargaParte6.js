6ca8456dd374c059a7a5447d8aa8a46da28d4d0c
philipheferreira <philipheferreira@gmail.com>
2024-09-16 18:00:12 -0300

    Feat: big project part 6 plano

---
+++ 4 - Mão na massa/5ConstruindoTestedeCargaParte6.js
@@ -0,0 +1,9 @@
+/* 
+	CRITÉRIOS
+		* Realizar consulta a API de listagem ed crocodilos e busca por id de crocodilos;
+		* É esperado um RPS de 200REQ/S para a API de listagem de crocodilos durante 30 seg;
+		* Para a busca por id, o sistema deve atender 50 usuários onde cada usuário realiza até 20 solicitações em até 1 minto
+			- Usuários par devem realizar buscar ao crocodilo de ID 2;
+			- Usuário ímpar devem realizar buscar ao crododilo de ID 1;
+		* Ambos os testes devem ser executados simultaneamente
+*/
