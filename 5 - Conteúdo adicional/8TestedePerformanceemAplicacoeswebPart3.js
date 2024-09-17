419c3e719203e15ae76ca89816328a09f4a0dd2f
philipheferreira <philipheferreira@gmail.com>
2024-09-17 18:00:13 -0300

    Feat: wbe aplication part 3 metrics information

--- 5 - Conteúdo adicional/8TestedePerformanceemAplicacoeswebPart3.js
+++ 5 - Conteúdo adicional/8TestedePerformanceemAplicacoeswebPart3.js
@@ -1,5 +1,7 @@
 /* 
-browser_web_vital_cls é a medida do maiot boost de pontuação de troca de layout
+browser_web_vital_cls é a medida do maiot boost de pontuação de troca de layout para cada mudança inesperada que ocorre durante toda a vida util de uma pagina
 
+browser_web_Beatles_LCP é a metrica que informa o tempo de renderização da maior imagem ou bloco de texto visivel na janela de visualização da pagina, em relação ao momento em que a pagina começou a ser carregada
 
+browser_web_vital_fid é a metrica que mede o tempo entre a primeira interação do usuario com a pagina até o momento em que o navegador consegue começar a processar os manipuladores de evento de interação com o usuario
 */
