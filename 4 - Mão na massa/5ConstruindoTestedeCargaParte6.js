86cc9b2c3c5799be75ce17f34ff88c6a96f8ab87
philipheferreira <philipheferreira@gmail.com>
2024-09-17 10:24:23 -0300

    Feat: big project part 6 create scenario listar and buscar

--- 4 - Mão na massa/5ConstruindoTestedeCargaParte6.js
+++ 4 - Mão na massa/5ConstruindoTestedeCargaParte6.js
@@ -11,7 +11,27 @@
 import http from 'k6';
 
 export const options = {
-
+	scenarios:{
+		listar: {
+			executor: 'const-arrival-rate',
+			exec: 'listar',
+			duration: '30s',
+			rate: 200,
+			timeUnit: '1s',
+			preAlocatedVud: 150,
+			gracefulStop: '10s',
+			tags: {test_type: 'listagem_de_crocodilos'}
+		},
+		buscar: {
+			executor:'per-vu-iteration',
+			exec: 'buscar',
+			vus: 50,
+			iterations: 20,
+			maxDuration: '1m',
+			gracefulStop: '10s',
+			tags: {test_type: 'busca_de_crocodilos'}
+		}
+	}
 
 };
 
@@ -20,5 +40,9 @@ export function listar(){
 }
 
 export function buscar(){
-
+	if(__VU % 2 === 0){
+		http.get(__ENV.URL + '/crocodiles/2');
+	}else{
+		http.get(__ENV.URL + '/crocodiles/1');
+	}
 }
