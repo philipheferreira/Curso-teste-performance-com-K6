c1683edd384798f25a39fc4136f8ff605ed042dc
philipheferreira <philipheferreira@gmail.com>
2024-09-17 18:32:16 -0300

    Feat: web aplications part 3 finish

--- 5 - Conteúdo adicional/8TestedePerformanceemAplicacoeswebPart3.js
+++ 5 - Conteúdo adicional/8TestedePerformanceemAplicacoeswebPart3.js
@@ -5,3 +5,49 @@ browser_web_Beatles_LCP é a metrica que informa o tempo de renderização da ma
 
 browser_web_vital_fid é a metrica que mede o tempo entre a primeira interação do usuario com a pagina até o momento em que o navegador consegue começar a processar os manipuladores de evento de interação com o usuario
 */
+
+import { browser } from 'k6/experimental/browser'
+import { sleep, check } from 'k6';
+
+export const options = {
+	scenarios: {
+		ui: {
+			executor: 'constant-vus',
+			vus: 3,
+			duration: '10s',
+			options: {
+				browser: {
+					type: 'chromium',
+				},
+			},
+		},
+	},
+	thresholds: {
+    checks: ['rate==1.0'],
+    browser_web_vital_fid: ["p(75) <= 100"],
+    browser_web_vital_lcp: ["p(75) <= 2500"],
+  },
+  summaryTrendStats: ["min","med","avg","max","p(75)","p(95)","p(99)"],
+}
+
+export default async function () {
+const page = browser.newPage();
+
+	try {
+		await page.goto('https://test.k6.io/my_messages.php');
+
+		page.locator('input[name="login"]').type('admin');
+		page.locator('input[name="password"]').type('123');
+
+		const submitButton = page.locator('input[type="submit"]'); 
+
+		await Promise.all([submitButton.click(), page.waitForNavigation()]);
+
+		check(page, {
+			header: (p) => p.locator('h2').textContent() == 'Welcome, admin!',
+		});
+
+	} finally {
+		page.close();
+	}
+}
