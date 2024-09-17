6f68d6eb4629db5dde54d5ed02843386c17735fc
philipheferreira <philipheferreira@gmail.com>
2024-09-17 17:49:01 -0300

    Feat: finish web aplication part 2

--- 5 - Conteúdo adicional/7TestedePerformanceemAplicacoeswebPart2.js
+++ 5 - Conteúdo adicional/7TestedePerformanceemAplicacoeswebPart2.js
@@ -8,16 +8,44 @@ export const options = { // para realizar testes web é obrigatorio a necessidad
 		ui: {
 			executor: 'constant-vus',
 			vus: 3,
-			durations: '10s',
+			duration: '10s',
 			options: {
 				browser: {// define o tipo de browser que vai ser aberto ou utilizado
 					type: 'chromium',
-				}
-			}
-		}
-	}
-	thresholds: {//Unicos limites a serem definidos é os checks
+				},
+			},
+		},
+	},
+	thresholds: {//Unicos limites a serem definidos é os checks, que todos tem que ocorrer
 		checks: ['rate == 1.0']
+	},
+};
+
+
+export default async function(){// a chamada precisa ser assincrona, usando o comando async, para que o codigo aguarde as respostas das aplicações web
+	const page = browser.newPage();// a variavel do tipo browzer confirma que vamos abrir uma nova pagina
+
+	try{
+
+		await page.goto('https://test.k6.io/my_messages.php');// vai dar um page.goto pra redirecionar para a pagina de interesse
+
+		// o page.locator é para especionar a tela que abrimos e capturar o elemento que queremos interagir
+
+		page.locator('input[name="login"]').type('admin'); // locator serve para acessar os elementos da tela e realizar testes pelo front
+		page.locator('input[name="password"]').type('123');// comando type serve para escrever no elemento localizado
+
+		const submitbutton = page.locator('input[type="submit"]'); // captura o elemento input do tipo submit e coloca ele dentro de uma constante
+
+		await Promise.all([submitbutton.click(), page.waitForNavigation()]); //define uma promise que vai clicar no botao que está dentro da variavel e clicar nele, e com isso vai aguardar a pagina carregar
+
+		check(page, {// cria um check pra confirmar que a operação ocorre corretamente, pois esse texto é da pagina seguinte
+			header: (p) => p.locator('h2').textContent() == 'Welcome, admin!',
+		});
+
+		sleep(1)
+		}finally{
+
+		page.close();
+
 	}
 }
-
