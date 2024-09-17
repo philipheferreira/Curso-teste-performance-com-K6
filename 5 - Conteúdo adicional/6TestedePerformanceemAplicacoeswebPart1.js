6b43c7004733aa23c9484f1efde8f1271dce28ea
philipheferreira <philipheferreira@gmail.com>
2024-09-17 13:04:13 -0300

    Feat: finish aplicacoes web first part

---
+++ 5 - Conteúdo adicional/6TestedePerformanceemAplicacoeswebPart1.js
@@ -0,0 +1,27 @@
+/*
+DESEMPENHO FRONTEND E BACKEND
+
+Desempenho frontend: 
+1 - Se as páginas do aplicativo são otimizadas para 
+renderizar rapidamente na tela de um usuário.
+2 - Quanto tempo leva para um usuário interagir 
+com os elementos de interface do aplicativo
+
+* Navegador
+	- O teste de carga baseado em navegador verifica o desempenho do
+	frontend de um aplicativo simulando usuários reais usando um
+	navegador para acessar seu site 
+
+* Protocolo 
+	- O teste de carga baseado em protocolo verifica o desempenho do
+	backend de um aplicativo simulando as solicitações subjacentes às
+	ações do usuário. -> quanto tempo demora para o backend retornar
+	ações solicitadas no frontend
+* Híbrido
+	- O teste de carga híbrido é uma combinação de teste de carga 
+	em protocolo é baseado em navegador
+
+
+* Como é feito o teste web? Ele é feito utilizando um modulo do k6,
+precisa apenas estar em uma versão superior ao 0.43.0
+*/
